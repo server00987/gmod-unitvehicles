6a5210f8934e79f9b5aaf74925c1c11827e0430e
MokaAkashiya85 <ThaERROR404@gmail.com>
2025-07-12 01:34:07 +0200

    Update uvracehandler.lua

--- lua/autorun/uvracehandler.lua
+++ lua/autorun/uvracehandler.lua
@@ -1516,12 +1516,71 @@ else
 		end
 
 		-- Racer nametags
-		if not UVHUDDisplayPursuit then
-			for vehicle, _ in pairs(UVHUDRaceInfo['Participants']) do
-				UVRenderEnemySquare( vehicle )
+		if RacerTags:GetBool() then
+			local localPlayer = LocalPlayer()
+			local myVehicle = localPlayer:GetVehicle()
+			if not IsValid(myVehicle) then return end
+
+			local renderQueue = {}
+
+			for vehicle, info in pairs(UVHUDRaceInfo.Participants or {}) do
+				if not IsValid(vehicle) or vehicle == myVehicle then continue end
+				if info.Status == "Disqualified" or info.Status == "Busted" then continue end
+
+				local dist = localPlayer:GetPos():DistToSqr(vehicle:GetPos())
+				table.insert(renderQueue, { vehicle = vehicle, dist = dist })
+			end
+
+			-- Sort so farther ones draw first, closer ones last (on top)
+			table.sort(renderQueue, function(a, b)
+				return a.dist < b.dist
+			end)
+
+			local maxSquares = 4
+			local count = math.min(#renderQueue, maxSquares)
+
+			for i = count, 1, -1 do
+				local data = renderQueue[i]
+				if data and IsValid(data.vehicle) then
+					UVRenderEnemySquare(data.vehicle)
+				end
 			end
 		end
+		
+		-- Racer Map Icons
+		for vehicle, info in pairs(UVHUDRaceInfo.Participants or {}) do
+			if not IsValid(vehicle) or vehicle == myVehicle then continue end
 
+			if not GMinimap then continue end
+			if not vehicle.displayedonhud then
+				vehicle.displayedonhud = true
+				local blip, id = GMinimap:AddBlip({
+					id = "UVBlip" .. vehicle:EntIndex(),
+					parent = vehicle,
+					icon = "unitvehicles/icons/MINIMAP_ICON_CAR.png",
+					scale = 1.4,
+					color = Color(255, 191, 0),
+				})
+				if vehicle:GetClass() == "prop_vehicle_jeep" then
+					blip.icon = "unitvehicles/icons/MINIMAP_ICON_CAR_JEEP.png" -- Icon points the other way
+				end
+			end
+
+			if vehicle.displayedonhud then
+				local curblip = GMinimap:FindBlipByID("UVBlip" .. vehicle:EntIndex())
+				if not curblip then continue end
+
+				if UVHUDDisplayCooldown or 
+				   (UVHUDCopMode and 
+					(tonumber(UVUnitsChasing) <= 0 or not vehicle.inunitview) and 
+					not ((not GetConVar("unitvehicle_unit_onecommanderevading"):GetBool()) and UVOneCommanderActive)) 
+				then
+					curblip.alpha = 0
+				else
+					curblip.alpha = 255
+				end
+			end
+		end
 		local racer_count = #string_array
 
 		local lang = language.GetPhrase
