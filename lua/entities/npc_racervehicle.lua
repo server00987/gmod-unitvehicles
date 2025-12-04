0002eb3f0349545cb1f0544a40ba860e1b8dd58b
Roboboy <keithleebryan12@gmail.com>
2025-12-04 13:54:04 +0800

    Tweak Throttle Control Logic

--- lua/entities/npc_racervehicle.lua
+++ lua/entities/npc_racervehicle.lua
@@ -528,8 +528,8 @@ if SERVER then
 			
 			-- print("Velocity:",selfvelocity)
 			
-			if self.v.IsSimfphyscar then
-				if selfvelocity > 10000 then
+			if GetConVar("unitvehicle_tractioncontrol"):GetBool() and selfvelocity > 10000 and not self.stuck then
+				if self.v.IsSimfphyscar then 
 					if istable(self.v.Wheels) then
 						for i = 1, table.Count( self.v.Wheels ) do
 							local Wheel = self.v.Wheels[ i ]
@@ -539,38 +539,23 @@ if SERVER then
 							end
 						end
 					end
+				elseif self.v.IsGlideVehicle then
+					local maxSlip = 0
+					for _, wheel in ipairs(self.v.wheels) do
+						maxSlip = math.max(maxSlip, math.abs(wheel:GetForwardSlip() or 0))
+					end
+					local minThrottle = 0.5
+					local recoverRate = FrameTime()
+					self.AI_ThrottleMul = self.AI_ThrottleMul or 1
+					if maxSlip > 8 then
+						self.AI_ThrottleMul = math.max(self.AI_ThrottleMul - FrameTime()*2, minThrottle)
+					else
+						self.AI_ThrottleMul = math.min(self.AI_ThrottleMul + recoverRate, 1)
+					end
+					throttle = throttle * self.AI_ThrottleMul --Glide traction control
+					self.usenitrous = self.AI_ThrottleMul == 1 and true or false
 				end
-			-- elseif self.v.IsGlideVehicle then
-			-- 	local EntityMeta = FindMetaTable( "Entity" )
-			-- 	local getTable = EntityMeta.GetTable
-			-- 	local selfvTbl = getTable( self.v )
-			-- 	local wheelslip = selfvTbl.avgForwardSlip > 0 and selfvTbl.avgForwardSlip or selfvTbl.avgForwardSlip < 0 and selfvTbl.avgForwardSlip * -1
-			-- 	if wheelslip ~= false then
-			-- 		throttle = throttle - (wheelslip/10) --Glide traction control
-			-- 	end
 			end
-			
-			-- if selfvelocity > 10000 then
-			-- 	if self.v.IsSimfphyscar then 
-			-- 		if istable(self.v.Wheels) then
-			-- 			for i = 1, table.Count( self.v.Wheels ) do
-			-- 				local Wheel = self.v.Wheels[ i ]
-			-- 				if not Wheel then return end
-			-- 				if Wheel:GetGripLoss() > 0 then
-			-- 					throttle = throttle * Wheel:GetGripLoss() --Simfphys traction control
-			-- 				end
-			-- 			end
-			-- 		end
-			-- 	elseif self.v.IsGlideVehicle then
-			-- 		local EntityMeta = FindMetaTable( "Entity" )
-			-- 		local getTable = EntityMeta.GetTable
-			-- 		local selfvTbl = getTable( self.v )
-			-- 		local wheelslip = selfvTbl.avgForwardSlip > 0 and selfvTbl.avgForwardSlip or selfvTbl.avgForwardSlip < 0 and selfvTbl.avgForwardSlip * -1
-			-- 		if wheelslip ~= false then
-			-- 			throttle = throttle - (wheelslip/10) --Glide traction control
-			-- 		end
-			-- 	end
-			-- end
 			
 			if dist:Dot(forward) < 0 and not self.stuck then
 				if vectdot > 0 then
@@ -607,21 +592,6 @@ if SERVER then
 					CFtoggleNitrous( self.v, self.usenitrous )
 				end
 				self.v:TriggerInput("Handbrake", 0)
-				if GetConVar("unitvehicle_tractioncontrol"):GetBool() and self.v.wheels then
-					local maxSlip = 0
-					for _, wheel in ipairs(self.v.wheels) do
-						maxSlip = math.max(maxSlip, math.abs(wheel:GetForwardSlip() or 0))
-					end
-					local minThrottle = 0.5
-					local recoverRate = FrameTime()
-					self.AI_ThrottleMul = self.AI_ThrottleMul or 1
-					if maxSlip > 8 then
-						self.AI_ThrottleMul = math.max(self.AI_ThrottleMul - FrameTime()*2, minThrottle)
-					else
-						self.AI_ThrottleMul = math.min(self.AI_ThrottleMul + recoverRate, 1)
-					end
-					throttle = throttle * self.AI_ThrottleMul
-				end
 				self.v:TriggerInput("Throttle", throttle)
 				self.v:TriggerInput("Brake", throttle * -1)
 				steer = steer * ((self.v.uvraceparticipant and 1.5) or 2) --Attempt to make steering more sensitive.
